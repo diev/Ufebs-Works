298d78a657c5c9fea701d99521387b947a64e5f1
Dmitrii Evdokimov <diev@mail.ru>
2022-08-05 16:05:06 +0300

    Add base code

--- Return-SWIFT/Program.cs
+++ Return-SWIFT/Program.cs
@@ -17,12 +17,107 @@ limitations under the License.
 */
 #endregion
 
+using System.Text;
+
 namespace Return_SWIFT;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string path, outPath;
+
+        if (args.Length == 0 || args[0] == "/?" || args[0] == "-?") // nothing
+        {
+            Console.WriteLine("Usage: Input|* [Output_]");
+            return;
+        }
+        else // Input specified
+        {
+            path = Path.GetFullPath(args[0]);
+        }
+
+        if (args.Length == 1) // Output default
+        {
+            string p = Path.GetDirectoryName(path) ?? Directory.GetCurrentDirectory();
+            outPath = Path.EndsInDirectorySeparator(p) ? p : p + Path.DirectorySeparatorChar;
+        }
+        else if (Directory.Exists(args[1]))
+        {
+            string p = args[1];
+            outPath = Path.EndsInDirectorySeparator(p) ? p : p + Path.DirectorySeparatorChar;
+        }
+        else
+        {
+            outPath = args[1];
+        }
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); //enable Windows-1251
+
+        if (Directory.Exists(path))
+        {
+            foreach (string file in Directory.GetFiles(path))
+            {
+                ProcessFile(file, outPath);
+            }
+        }
+        else if (path.Contains('*') || path.Contains('?'))
+        {
+            string dir = Path.GetDirectoryName(path) ?? Directory.GetCurrentDirectory();
+            string mask = Path.GetFileName(path) ?? "*";
+
+            foreach (string file in Directory.GetFiles(dir, mask))
+            {
+                ProcessFile(file, outPath);
+            }
+        }
+        else if (File.Exists(path))
+        {
+            ProcessFile(path, outPath);
+        }
+        else
+        {
+            Console.WriteLine($"Input \"{path}\" not found");
+        }
+
+        #region finish
+        while (true)
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                break;
+            }
+        }
+        #endregion finish
+    }
+
+    private static void ProcessFile(string inFile, string outFile)
+    {
+        if (Path.EndsInDirectorySeparator(outFile)) // Directory.Exists(outFile)
+        {
+            var file = new FileInfo(inFile);
+            int len = file.Extension.Length;
+            outFile = Path.Combine(outFile, file.Name[..^len] + "_" + file.Extension);
+        }
+
+        if (File.Exists(outFile))
+        {
+            Console.WriteLine($"Output \"{outFile}\" overwritten");
+        }
+
+        //TODO
+
+        Console.WriteLine();
+
+        if (File.Exists(outFile))
+        {
+            Console.WriteLine($"[Output \"{outFile}\" done. Press Spacebar.]");
+        }
+        else
+        {
+            Console.WriteLine($"[Output \"{outFile}\" FAIL! Press Spacebar.]");
+        }
+
+        Console.WriteLine();
     }
 }
