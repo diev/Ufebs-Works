2a4e681b22f73df1e6fc01fa8875f8a999840027
Dmitrii Evdokimov <diev@mail.ru>
2022-08-12 05:14:33 +0300

    Add new Tests

---
+++ Corr-SWIFT.Tests/SWIFT/SwiftParsersTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CorrLib.SWIFT;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrLib.SWIFT.Tests
+{
+    [TestClass()]
+    public class SwiftParsersTests
+    {
+        [TestMethod()]
+        public void ToSwiftDateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
